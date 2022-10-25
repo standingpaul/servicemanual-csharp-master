63b2c303cf05c86e28152e0ef04648a193e18313
Paul Standing <paul@flatfishgames.com>
2022-10-25 21:19:47 +0300

    Created new template and add the Etteplan scripts

---
+++ ServiceManual/Controllers/Class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceManual.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceManualController
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            Console.WriteLine($"Get");
+
+            return "Worked";
+        }
+    }
+}
