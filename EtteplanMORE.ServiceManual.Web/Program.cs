b0b3a498a083a0c62ac9fad86a8aaa475fb37b4f
Paul Standing <paul@flatfishgames.com>
2022-10-26 14:16:16 +0300

    Rework project setup

--- EtteplanMORE.ServiceManual.Web/Program.cs
+++ EtteplanMORE.ServiceManual.Web/Program.cs
@@ -9,8 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<IFactoryDeviceService, FactoryDeviceService>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
